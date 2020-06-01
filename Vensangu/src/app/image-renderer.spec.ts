009ae6b1d26c9cf5979bd461456d31b858c11500
Saravanandss <Saravanandss@gmail.com>
2020-06-01 15:50:51 -0500

    Removing Angular based Vensangu project

--- Vensangu/src/app/image-renderer.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ImageRenderer } from './image-renderer';
-
-describe('ImageRenderer', () => {
-  it('should create an instance', () => {
-    expect(new ImageRenderer()).toBeTruthy();
-  });
-});
